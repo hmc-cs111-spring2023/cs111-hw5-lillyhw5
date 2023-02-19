c4aaf3d657eb63c8fb826a0b7d7edc15d1916925
Lilly Lee <lillee@pop-os.localdomain>
2023-02-19 13:48:52 -0800

    Added comments

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -1,21 +1,31 @@
 package dfa // leave this line in the file
 
 class DFA(val states: Set[State], val start: State, val accept: Set[State], val transitions: Set[Transition]):
+    /** Returns true if DFA accepts input, false otherwise. 
+      */
     def accepts(input: String): Boolean = input match {
-        case "" => accept.contains(start) // if empty string, return true if start state is also an accepting state; false otherwise
-        case _ => acceptsHelper((input, start))
+        case "" => accept.contains(start) // if empty string, return whether or not start state is an accepting state
+        case _ => acceptsHelper(input, start)
     }
 
-    // recursive helper for accepts function
-    def acceptsHelper(tup: (String, State)): Boolean = {
-        lazy val Trans = returnTransition(tup(0)(0), tup(1)) // retrieve the transition that takes 
-        tup match {
-            case ("", currState) => accept.contains(currState) 
-            case (input, currState) if !Trans.isEmpty => acceptsHelper((input.substring(1), Trans.head.to))
-            case (input, currState) => false 
+    /** Recursive helper for accepts function which returns Boolean value
+      * if DFA is accepted, false otherwise.
+      * Has additional parameter currState that keeps track of the current state 
+      */
+    def acceptsHelper(input: String, currState: State): Boolean = {
+        lazy val Trans = returnTransition(input(0), currState) // retrieve the transition that takes 
+        input match {
+            case "" => accept.contains(currState) // if empty string, return whether or not current state is an accepting state
+            case input if !Trans.isEmpty => acceptsHelper(input.substring(1), Trans.head.to) // recurse if there exists valid transition
+                                                                                             // currState is set to what the transition leads to
+            case _ => false
         }
     }
 
+    /** Helper function that fetches transition from the set transitions that
+      * satisfies from = currState and symbol = char
+      * if such transition does not exist, returns empty set
+      */
     def returnTransition(char: Char, currState: State) = {
         val trans = transitions.filter(x => x.from == currState && x.symbol == char )
         trans 
