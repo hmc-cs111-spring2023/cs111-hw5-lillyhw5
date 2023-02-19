760ad0deaffa267314da7adabaf883fdd05c3c96
Lilly Lee <lillee@pop-os.localdomain>
2023-02-19 13:33:43 -0800

    finished coding

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -1,3 +1,26 @@
 package dfa // leave this line in the file
 
-// TODO: replace this comment with your implementation
+class DFA(val states: Set[State], val start: State, val accept: Set[State], val transitions: Set[Transition]):
+    def accepts(input: String): Boolean = input match {
+        case "" => accept.contains(start) // if empty string, return true if start state is also an accepting state; false otherwise
+        case _ => acceptsHelper((input, start))
+    }
+
+    // recursive helper for accepts function
+    def acceptsHelper(tup: (String, State)): Boolean = {
+        lazy val Trans = returnTransition(tup(0)(0), tup(1)) // retrieve the transition that takes 
+        tup match {
+            case ("", currState) => accept.contains(currState) 
+            case (input, currState) if !Trans.isEmpty => acceptsHelper((input.substring(1), Trans.head.to))
+            case (input, currState) => false 
+        }
+    }
+
+    def returnTransition(char: Char, currState: State) = {
+        val trans = transitions.filter(x => x.from == currState && x.symbol == char )
+        trans 
+    }
+            
+case class State(label: String)
+
+case class Transition(from: State, to: State, symbol: Char)
--- regex/src/main/scala/regex/Regex.scala
+++ regex/src/main/scala/regex/Regex.scala
@@ -6,7 +6,14 @@ package regex
   * data structure definitions for regular languages
   */
 
-// Add your definitions here
+trait RegularLanguage
+
+case object Empty extends RegularLanguage
+case object Epsilon extends RegularLanguage
+case class Character(char: Char) extends RegularLanguage
+case class Union(r1: RegularLanguage, r2: RegularLanguage) extends RegularLanguage
+case class Concat(r1: RegularLanguage, r2: RegularLanguage) extends RegularLanguage
+case class Star(r: RegularLanguage) extends RegularLanguage
 
 /** *****************************************************************************
   * Derivatives
@@ -15,13 +22,44 @@ package regex
   */
 
 /** Simplifies a regular language */
-def simplify(lang: RegularLanguage): RegularLanguage = ???
+def simplify(lang: RegularLanguage): RegularLanguage = lang match {
+  case Concat(Epsilon, r: RegularLanguage) => simplify(r)
+  case Concat(r: RegularLanguage, Epsilon) => simplify(r)
+  case Concat(Empty, r: RegularLanguage) => simplify(Empty)
+  case Concat(r: RegularLanguage, Empty) => simplify(Empty)
+  case Concat(r1: RegularLanguage, r2: RegularLanguage) => Concat(simplify(r1), simplify(r2))
+
+  case Union(Empty, r: RegularLanguage) => simplify(r)
+  case Union(r: RegularLanguage, Empty) => simplify(r)
+  case Union(r1: RegularLanguage, r2: RegularLanguage) => Union(simplify(r1), simplify(r2))
+  
+  case Star(Epsilon) => Epsilon
+  case Star(Empty) => Empty
+  case Star(r: RegularLanguage) => Star(simplify(r))
+
+  case _ => lang
+}
 
 /** A language is nullable if it contains ε */
-def nullable(lang: RegularLanguage): Boolean = ???
+def nullable(lang: RegularLanguage): Boolean = 
+  val simpLang = simplify(lang)
+  simpLang match {
+    case Epsilon => true 
+    case Star(r: RegularLanguage) => true 
+    case _ => false 
+  }
 
 /** Computes the derivative of a language, with respect to a character */
-def derivative(l: RegularLanguage)(c: Char): RegularLanguage = ???
+def derivative(l: RegularLanguage)(c: Char): RegularLanguage = l match {
+  case Empty => Empty 
+  case Epsilon => Empty 
+  case Character(d) if (c == d) => Epsilon
+  case Character(d) => Empty 
+  case Union(r1: RegularLanguage, r2: RegularLanguage) => Union(derivative(r1)(c), derivative(r2)(c))
+  case Concat(r1: RegularLanguage, r2: RegularLanguage) if !nullable(r1) => Concat(derivative(r1)(c), r2)
+  case Concat(r1: RegularLanguage, r2: RegularLanguage) => Union(Concat(derivative(r1)(c), r2), derivative(r2)(c))
+  case Star(r: RegularLanguage) => Concat(derivative(r)(c), Star(r))
+}
 
 /** *****************************************************************************
   * String-matching with regular expressions
